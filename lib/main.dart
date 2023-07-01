cc44ba02cf2390a98d0859b69d228843c64c83fd
Arman Bakirov <52448072+Lokki16@users.noreply.github.com>
2023-07-01 19:13:22 +0600

    update

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:test_lunapp/app.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
+
+  SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+  ]);
 
   runApp(const LunApp());
 }
